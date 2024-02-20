8ed314ca0cb1e01faa9e4a5c095fc02b132cf872
sivareddy27 <157670088+sivareddy27@users.noreply.github.com>
2024-02-20 10:45:34 -0600

    Update ex_4_2.py

--- src/ex_4_2.py
+++ src/ex_4_2.py
@@ -6,7 +6,7 @@ def logstamp_to_datetime(datestr):
     """
     Create a datetime object from a string containing the date and time.
     """
-    return datetime.strptime(datestr, '%Y-%m-%dT%H:%M;%S')
+    return datetime.strptime(datestr, '%Y-%m-%dT%H:%M:%S')
 
 
 # >>>> The code below will call your function and print the results
